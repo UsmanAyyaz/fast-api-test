4b2351db533ea47dee0a8d901a35a6c4340f638f
Usman Ayyaz <usmayyaz@gmail.com>
2024-02-28 14:01:53 -0800

    created static folder

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = FastAPI()
 
 favicon_path = 'favicon.ico'
 
+from fastapi.staticfiles import StaticFiles
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 @app.get('/favicon.ico', include_in_schema=False)
 async def favicon():
     return FileResponse(favicon_path)
